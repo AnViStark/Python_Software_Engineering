7f7f5604b5b5e0e6cd934f9311df5d7e8785c77a
Andrey Vinokurov <ankavistark@gmail.com>
2024-09-24 21:54:03 +0500

    added Tema_3.md

--- Py3/sam3_4.py
+++ Py3/sam3_4.py
@@ -1,21 +1,16 @@
 sentence = input("Введите предложение: ")
-
 length = 0
 for i in sentence:
     length += 1
 print("Длина предложения:", length)
-
-
 low_sentence = sentence.lower()
 print("Предложение в нижнем регистре: ", low_sentence)
-
 vowels = "aeiou"
 count = 0
 for j in low_sentence:
     if j in vowels:
         count += 1
 print("Количество гласных: ", count)
-
 beauty_sentence = ""
 i = 0
 while i < length:
@@ -26,7 +21,6 @@ while i < length:
         beauty_sentence += low_sentence[i]
         i += 1
 print("Предложение после замены: ", beauty_sentence)
-
 if low_sentence[:3] == "the" and low_sentence[-3:] == "end":
     print("Предложение начинается с The и заканчивается на end")
 else:
