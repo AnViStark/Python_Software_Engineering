f524fd0c8715e7212431a07043b38f57e575b0a1
Andrey Vinokurov <ankavistark@gmail.com>
2024-10-13 16:32:34 +0500

    some fixes

--- Py6/sam6_5.py
+++ Py6/sam6_5.py
@@ -1,30 +1,28 @@
-def finder(t):
-    max_inc = [t[0]]
-    max_dec = [t[0]]
-    tmp_inc = [t[0]]
-    tmp_dec = [t[0]]
+def finder(tpl):
+    max_inc = [tpl[0]]
+    max_dec = [tpl[0]]
+    tmp_inc = [tpl[0]]
+    tmp_dec = [tpl[0]]
     
-    for i in range(1, len(t)):
-        if t[i] > t[i - 1]:
-            tmp_inc.append(t[i])
+    for i in range(1, len(tpl)):
+        if tpl[i] > tpl[i - 1]:
+            tmp_inc.append(tpl[i])
             if len(tmp_inc) > len(max_inc):
                 max_inc = tmp_inc[:]
-            tmp_dec = [t[i]]
-        elif t[i] < t[i - 1]:
-            tmp_dec.append(t[i])
+            tmp_dec = [tpl[i]]
+        elif tpl[i] < tpl[i - 1]:
+            tmp_dec.append(tpl[i])
             if len(tmp_dec) > len(max_dec):
                 max_dec = tmp_dec[:]
-            tmp_inc = [t[i]]
+            tmp_inc = [tpl[i]]
 
     if not max_inc:
-        max_inc = [t[0]]
+        max_inc = [tpl[0]]
     if not max_dec:
-        max_dec = [t[0]]
+        max_dec = [tpl[0]]
 
     return tuple(max_inc), tuple(max_dec)
 
 print(finder((1, 2, 3, 2, 1, 4, 5, 1, 2)))
 print(finder((5, 4, 3, 2, 1)))
 print(finder((1, 3, 5, 7, 9)))
-
-#Создайте программу, которая получает на вход кортеж из последовательности чисел. Программа должна находить максимальную и минимальную длину последовательных возрастающих и убывающих подпоследовательностей в исходном кортеже. Необходимо вернуть два кортежа: один с максимальной длиной возрастающей подпоследовательности, а второй — с максимальной длиной убывающей подпоследовательности.
